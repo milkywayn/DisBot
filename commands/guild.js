d5bee2bfb8831cbd90ff619c621e32dd387b7ca8
milkywayn <milkywayn@users.noreply.github.com>
2026-02-01 12:58:11 +0900

    war count bold eg

--- commands/guild.js
+++ commands/guild.js
@@ -130,7 +130,7 @@ module.exports = {
         const wars = p.wars ?? 0;
 
         // wars 1000以上は強調
-        const warsText = wars >= 1000 ? `**${wars} wars**` : `${wars} wars`;
+        const warsText = wars >= 1000 ? `**${wars}** wars` : `${wars} wars`;
         onlineText += `• ${p.name} (${p.server} | ${p.wars} wars)\n`;
       }
       onlineText += "\n";
